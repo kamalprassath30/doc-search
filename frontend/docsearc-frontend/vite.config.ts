ed6c5841c9c58d59c9766c06c1b729c570762b52
kamalprassath30 <kamalprasath2003@gmail.com>
2026-01-04 19:31:24 +0530

    sdd

--- frontend/docsearc-frontend/vite.config.ts
+++ frontend/docsearc-frontend/vite.config.ts
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    dedupe: ["react", "react-dom"], // avoids duplicate React in monorepo
-  },
   optimizeDeps: {
-    include: ["axios"], // <-- forces Vite to pre-bundle axios
+    include: ["axios"], // force Vite to pre-bundle axios
   },
-  base: "/", // ensures proper path on Vercel
+  base: "/",
 });
