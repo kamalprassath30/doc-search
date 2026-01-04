8e4f3bc5f7b8786d6da7a0188818ef460d72e1bc
kamalprassath30 <kamalprasath2003@gmail.com>
2026-01-04 10:44:40 +0530

    env fix

--- frontend/docsearc-frontend/src/api/api.ts
+++ frontend/docsearc-frontend/src/api/api.ts
@@ -2,9 +2,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  // baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
-
+console.log(import.meta.env.VITE_API_BASE_URL);
 export const uploadFile = (file) => {
   const formData = new FormData();
   formData.append("file", file);
